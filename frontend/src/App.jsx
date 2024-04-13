50e2c84a69fd0e0e9ae35ad4fda2c7f0e9c1c539
tulik <tolqinmirsaliyev@gmail.com>
2024-04-13 22:56:24 +0300

    fdfdf

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,10 @@
 import { useState } from 'react'
 import './index.css'
 import Navbar from './Components/Navbar'
+import Register from './Components/Register'
+import Login from './Components/Login'
+import {Routes, Route}  from 'react-router-dom'
+
 
 
 function App() {
@@ -9,6 +13,14 @@ function App() {
   return (
     <>
     <Navbar/>
+    
+    <Routes>
+      <Route path='/' element = {<Register/>}/>
+    </Routes>
+    <Routes>
+      <Route path='/login' element = {<Login/>}/>
+    </Routes>
+
        
     </>
   )
--- frontend/src/Components/Register.jsx
+++ frontend/src/Components/Register.jsx
@@ -2,7 +2,33 @@ import React from 'react'
 
 const Register = () => {
   return (
-    <div>Register</div>
+    <div>
+
+      <div class="h-screen bg-indigo-100 flex justify-center items-center">
+        <div class="lg:w-2/5 md:w-1/2 w-2/3">
+          <form class="bg-white p-10 rounded-lg shadow-lg min-w-full">
+            <h1 class="text-center text-2xl mb-6 text-gray-600 font-bold font-sans">Formregister</h1>
+            <div>
+              <label class="text-gray-800 font-semibold block my-3 text-md" for="username">Username</label>
+              <input class="w-full bg-gray-100 px-4 py-2 rounded-lg focus:outline-none" type="text" name="username" id="username" placeholder="username" />
+            </div>
+            <div>
+              <label class="text-gray-800 font-semibold block my-3 text-md" for="email">Email</label>
+              <input class="w-full bg-gray-100 px-4 py-2 rounded-lg focus:outline-none" type="text" name="email" id="email" placeholder="@email" />
+            </div>
+            <div>
+              <label class="text-gray-800 font-semibold block my-3 text-md" for="password">Password</label>
+              <input class="w-full bg-gray-100 px-4 py-2 rounded-lg focus:outline-none" type="text" name="password" id="password" placeholder="password" />
+            </div>
+            <div>
+              <label class="text-gray-800 font-semibold block my-3 text-md" for="confirm">Confirm password</label>
+              <input class="w-full bg-gray-100 px-4 py-2 rounded-lg focus:outline-none" type="text" name="confirm" id="confirm" placeholder="confirm password" />
+            </div>
+            <button type="submit" class="w-full mt-6 bg-indigo-600 rounded-lg px-4 py-2 text-lg text-white tracking-wide font-semibold font-sans">Register</button>
+            
+          </form>
+        </div>
+      </div></div>
   )
 }
 
