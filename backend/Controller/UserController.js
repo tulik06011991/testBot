3452f49aec1a6beaf0d7e538dbcd87b4d8002f00
realProject <tolqinmirsaliyev@gmail.com>
2024-04-20 09:39:34 +0500

    ggf

--- backend/Controller/UserController.js
+++ backend/Controller/UserController.js
@@ -27,10 +27,10 @@ const getUsers = async (req, res) => {
 const getUserId = async (req, res) => {
     try {
         const { id } = req.params;
-      const user = await AuthModel.findById(id);
-      if(!user){
-        res.status(400).send(`bunday ${id} foydalanuvchi topilmadi`)
-    }
+        const user = await AuthModel.findById(id);
+        if(!user){
+          res.status(400).send(`bunday  foydalanuvchi topilmadi`)
+      }
       res.status(200).json(user);
     } catch (error) {
       res.status(500).json({ error: error.message });
@@ -56,10 +56,10 @@ const updateUser = async (req, res) => {
 const deleteUser = async (req, res) => {
   try {
     const { id } = req.params;
-    await AuthModel.findByIdAndDelete(id);
     if(!id){
         res.status(400).send(` bunday ${id} foydalanuvchilar topilmadi`)
     }
+    await AuthModel.findByIdAndDelete(id);
     res.status(204).send();
   } catch (error) {
     res.status(500).json({ error: error.message });
