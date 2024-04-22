a83a4b2c28795b68cbedafa580670bf58f22220a
realProject <tolqinmirsaliyev@gmail.com>
2024-04-22 12:22:46 +0500

    sss

--- frontend/src/Components/Menu.jsx
+++ frontend/src/Components/Menu.jsx
@@ -1,7 +1,12 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { Link } from 'react-router-dom'
 
 const Menu = () => {
+
+  // useEffect(() =>{
+
+
+  // },[])
   return (
     <div>
 
