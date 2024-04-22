c68646efdd1940c898aad0dd5a44a37bf8723d01
realProject <tolqinmirsaliyev@gmail.com>
2024-04-22 10:43:12 +0500

    xx

--- frontend/src/Components/Login.jsx
+++ frontend/src/Components/Login.jsx
@@ -1,27 +1,55 @@
-import React from 'react'
+
+import React, { useState } from 'react';
+import axios from 'axios';
+import { Link, useNavigate } from 'react-router-dom';
 
 const Login = () => {
+
+  const navigate = useNavigate();
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  
+  const handleSubmitt = async (e) => {
+    e.preventDefault();
+    try {
+      const response = await axios.post(
+        'http://localhost:8800/auth/login',
+        { email: email, password: password },
+        {
+          headers: {
+            'Content-Type': 'application/json'
+          },
+          withCredentials: true
+        },
+      );
+      console.log(response.data)
+      if (response.data.isAdmin) {
+        navigate('/dashboard');
+      } else {
+        navigate('/userMain');
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
   return (
     <div>
       
-      <div class="h-screen bg-indigo-100 flex justify-center items-center">
-        <div class="lg:w-2/5 md:w-1/2 w-2/3">
-          <form class="bg-white p-10 rounded-lg shadow-lg min-w-full">
-            <h1 class="text-center text-2xl mb-6 text-gray-600 font-bold font-sans">Login</h1>
+      <div className="h-screen bg-indigo-100 flex justify-center items-center">
+        <div className="lg:w-2/5 md:w-1/2 w-2/3">
+          <form  onSubmit={handleSubmitt} className="bg-white p-10 rounded-lg shadow-lg min-w-full">
+            <h1 className="text-center text-2xl mb-6 text-gray-600 font-bold font-sans">Login</h1>
             
             <div>
-              <label class="text-gray-800 font-semibold block my-3 text-md" for="email">Email</label>
-              <input class="w-full bg-gray-100 px-4 py-2 rounded-lg focus:outline-none" type="text" name="email" id="email" placeholder="@email" />
+              <label className="text-gray-800 font-semibold block my-3 text-md" for="email">Email</label>
+              <input   onChange={(e) => setEmail(e.target.value)} className="w-full bg-gray-100 px-4 py-2 rounded-lg focus:outline-none" type="text" name="email" id="email" placeholder="@email" />
             </div>
             <div>
-              <label class="text-gray-800 font-semibold block my-3 text-md" for="password">Password</label>
-              <input class="w-full bg-gray-100 px-4 py-2 rounded-lg focus:outline-none" type="text" name="password" id="password" placeholder="password" />
+              <label className="text-gray-800 font-semibold block my-3 text-md" for="password">Password</label>
+              <input   onChange={(e) => setPassword(e.target.value)} className="w-full bg-gray-100 px-4 py-2 rounded-lg focus:outline-none" type="text" name="password" id="password" placeholder="password" />
             </div>
-            <div>
-              <label class="text-gray-800 font-semibold block my-3 text-md" for="confirm">Confirm password</label>
-              <input class="w-full bg-gray-100 px-4 py-2 rounded-lg focus:outline-none" type="text" name="confirm" id="confirm" placeholder="confirm password" />
-            </div>
-            <button type="submit" class="w-full mt-6 bg-indigo-600 rounded-lg px-4 py-2 text-lg text-white tracking-wide font-semibold font-sans">Login</button>
+            
+            <button type="submit" className="w-full mt-6 bg-indigo-600 rounded-lg px-4 py-2 text-lg text-white tracking-wide font-semibold font-sans">Login</button>
             
           </form>
         </div>
