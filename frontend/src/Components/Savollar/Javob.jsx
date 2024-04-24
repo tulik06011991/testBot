00645dc9329853613b001b4dcd97dec829832e17
realProject <tolqinmirsaliyev@gmail.com>
2024-04-24 12:18:16 +0500

    aaa

--- frontend/src/Components/Savollar/Javob.jsx
+++ frontend/src/Components/Savollar/Javob.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Javob = () => {
   return (
-    <div><br /><br /><br /><br /><h1 className='text-xl'>tamom</h1></div>
+    <div><br /><br /><br /><br /><h1 className='text-2xl font-bold'>Tamomladingiz : balingiz 200</h1></div>
   )
 }
 
