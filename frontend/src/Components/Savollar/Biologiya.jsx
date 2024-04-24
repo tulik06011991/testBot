4eb634d7ee2954ba415f9a7b99ba59d166a45a72
realProject <tolqinmirsaliyev@gmail.com>
2024-04-24 12:36:10 +0500

    sdsadsa

--- frontend/src/Components/Savollar/Biologiya.jsx
+++ frontend/src/Components/Savollar/Biologiya.jsx
@@ -60,7 +60,7 @@ const Biologiya = () => {
           <h2 className='font-bold text-xl'>{questions[currentQuestionIndex].title}</h2>
           <br /><br />
           {questions[currentQuestionIndex].options.map((option, index) => (
-            <div key={index} className='bg-blue-350 shadow  my-4 text-xl hover:bg-blue-500 py-6 rounded'>
+            <div key={index} className='bg-blue-350 shadow  my-2 text-xl hover:bg-blue-500 py-6 rounded'>
               <input
                 type="radio"
                 id={option}
