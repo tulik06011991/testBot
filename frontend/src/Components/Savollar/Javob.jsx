2f953c55d1a2e9133f3812c93eb3ba1b7b7c7d11
realProject <tolqinmirsaliyev@gmail.com>
2024-04-26 11:27:58 +0500

    aaa

--- frontend/src/Components/Savollar/Javob.jsx
+++ frontend/src/Components/Savollar/Javob.jsx
@@ -1,9 +1,13 @@
 import React, { useContext, useState } from 'react';
 import UserContext from '../userContext';
+import { Link } from 'react-router-dom';
+
 
 // Alloh o'zinga ming qadar shukr!   oxiri uddaladim
 const Javob = () => {
+  
   const { natija } = useContext(UserContext);
+
 
   const [data] = useState(natija);
   console.log(data)
@@ -11,7 +15,7 @@ const Javob = () => {
     <>
         <br /><br /><br /><br />
       <div className='flex-col justify-center items-center w-8/12'>
-        <h2 className='text-xl font-bold'> Savollarning umumiy soni:  {data.totalQuestions}</h2>
+        <h2 className='text-xl font-bold '> Savollarning umumiy soni:  {data.totalQuestions}</h2>
         <br /><br />
         <h1 className='text-xl font-bold'>
 
@@ -21,7 +25,7 @@ const Javob = () => {
         <h2 className='text-xl font-bold'> Umumiy balingiz : {data.userScore
         }</h2>
 
-
+          <button className='bg-blue-300  py-2 px-4 hover:bg-blue-500 text-white'><Link to={'/menu'}>Orqaga</Link></button>
       </div>
 
 
