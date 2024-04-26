9bbba7b322973a19966da6b0480b26aebcf4850e
realProject <tolqinmirsaliyev@gmail.com>
2024-04-26 12:21:13 +0500

    sss

--- frontend/src/Components/Savollar/Javob.jsx
+++ frontend/src/Components/Savollar/Javob.jsx
@@ -31,7 +31,7 @@ const Javob = () => {
           To'g'ri javoblar soni: {data.correctCount}
         </h1>
         <br /><br />
-        <h2 className='text-xl font-bold'> Umumiy balingiz: {data.userScore}</h2>
+        <h2 className='text-xl font-bold'> Umumiy balingiz: {data.userScore}</h2><br /><br /><br />
         <button className='bg-blue-300  py-2 px-4 hover:bg-blue-500 text-white'><Link to={'/menu'}>Orqaga</Link></button>
       </div>
     </>
