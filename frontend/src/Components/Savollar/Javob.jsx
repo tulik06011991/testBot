c755630a0fd1539b405d38094f844df5460d0a5d
realProject <tolqinmirsaliyev@gmail.com>
2024-04-27 10:10:46 +0500

    sss

--- frontend/src/Components/Savollar/Javob.jsx
+++ frontend/src/Components/Savollar/Javob.jsx
@@ -1,6 +1,7 @@
 import React, { useContext, useEffect, useState } from 'react';
 import UserContext from '../userContext';
 import { Link, useNavigate } from 'react-router-dom';
+import axios from 'axios';
 
 const Javob = () => {
   const { natija } = useContext(UserContext);
@@ -15,6 +16,21 @@ const Javob = () => {
     // Save the data to localStorage when it changes
     localStorage.setItem('javobData', JSON.stringify(data));
   }, [data]);
+
+  const handleClearLocalStorage = () => {
+    // Clear localStorage data
+    localStorage.removeItem('javobData');
+    
+    // Clear backend data
+    axios.delete('http://localhost:3000/test/deleteJavoblar')
+      .then(response => {
+        console.log(response.data); // Log the response from the server
+        // Additional logic if needed
+      })
+      .catch(error => {
+        console.error('Xatolik:', error); // Log any errors
+      });
+  };
 
   useEffect(() => {
     // This effect will execute only when the component mounts
@@ -32,7 +48,9 @@ const Javob = () => {
         </h1>
         <br /><br />
         <h2 className='text-xl font-bold'> Umumiy balingiz: {data.userScore}</h2><br /><br /><br />
-        <button className='bg-blue-300  py-2 px-4 hover:bg-blue-500 text-white'><Link to={'/menu'}>Orqaga</Link></button>
+        <button className='bg-blue-300  py-2 px-4 hover:bg-blue-500 text-white' onClick={handleClearLocalStorage}>
+          <Link to={'/menu'}>Orqaga</Link>
+        </button>
       </div>
     </>
   );
