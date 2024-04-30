9fd199f3766ecbe9285348a5e6df5b6e56716ffc
realProject <tolqinmirsaliyev@gmail.com>
2024-04-30 11:30:29 +0500

    ssss

--- frontend/src/Components/Savollar/Tarix.jsx
+++ frontend/src/Components/Savollar/Tarix.jsx
@@ -1,78 +1,9 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
+import React from 'react'
 
-const Quiz = () => {
-  const [questions, setQuestions] = useState([]);
-  const [currentQuestionIndex, setCurrentQuestionIndex] = useState(0);
-  const [userAnswers, setUserAnswers] = useState([]);
-  const [message, setMessage] = useState('');
-  
-  useEffect(() => {
-    // Savollarni serverdan olish
-    const fetchQuestions = async () => {
-      try {
-        const response = await axios.get('/api/questions');
-        setQuestions(response.data.questions);
-        // Foydalanuvchi javoblari uchun bo'sh massivni yaratish
-        setUserAnswers(Array(response.data.questions.length).fill(''));
-      } catch (error) {
-        console.error('Xatolik:', error);
-        setMessage('Savollarni yuklashda xatolik yuz berdi');
-      }
-    };
-    fetchQuestions();
-  }, []);
-  
-  const handleAnswerChange = (e) => {
-    const updatedUserAnswers = [...userAnswers];
-    updatedUserAnswers[currentQuestionIndex] = e.target.value;
-    setUserAnswers(updatedUserAnswers);
-  };
-  
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-    try {
-      // Foydalanuvchi javoblarni serverga yuborish
-      const response = await axios.post('http://localhost:3000/test/answer/post', {
-        userId: 'user_id', // Foydalanuvchi identifikatori
-        questionId: questions[currentQuestionIndex]._id,
-        userAnswer: userAnswers[currentQuestionIndex]
-      });
-      setMessage(response.data.message);
-      // Keyingi savolga o'tish
-      setCurrentQuestionIndex(currentQuestionIndex + 1);
-    } catch (error) {
-      console.error('Xatolik:', error);
-      setMessage('Javobni yuborishda xatolik yuz berdi');
-    }
-  };
-
+const Tarix = () => {
   return (
-    <div>
-      {questions.length > 0 ? (
-        <form onSubmit={handleSubmit}>
-          <h2>{questions[currentQuestionIndex].title}</h2>
-          {questions[currentQuestionIndex].options.map((option, index) => (
-            <div key={index}>
-              <input
-                type="radio"
-                id={option}
-                name="answer"
-                value={option}
-                onChange={handleAnswerChange}
-                checked={userAnswers[currentQuestionIndex] === option}
-              />
-              <label htmlFor={option}>{option}</label>
-            </div>
-          ))}
-          <button type="submit">Keyingi savol</button>
-        </form>
-      ) : (
-        <p>Savollar yuklanmoqda...</p>
-      )}
-      {message && <p>{message}</p>}
-    </div>
-  );
-};
+    <div> <br /><br /><br /><br /><h1>hali savollar joylanmagan</h1><Link to={'/menu'}  ><button className='bg-blue-500 text-xl py-2 px-4 mt-5 rounded'>Orqaga</button></Link></div>
+  )
+}
 
-export default Quiz;
+export default Tarix
