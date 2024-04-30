0fc2ef7794af70743d988d8fd24fe5bd7d4359fc
realProject <tolqinmirsaliyev@gmail.com>
2024-04-30 11:32:51 +0500

    dfdf

--- frontend/src/Components/Savollar/Tarix.jsx
+++ frontend/src/Components/Savollar/Tarix.jsx
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom'
 
 const Tarix = () => {
   return (
-    <div> <br /><br /><br /><br /> <Link to={'/menu'}><button className='bg-blue-500 text-xl py-2 px-4 mt-5 rounded'>Orqaga</button></Link></div>
+    <div> <br /><br /><br /><br /><h1>hali savollar  joylanmagan</h1> <Link to={'/menu'}><button className='bg-blue-500 text-xl py-2 px-4 mt-5 rounded'>Orqaga</button></Link></div>
   )
 }
 
