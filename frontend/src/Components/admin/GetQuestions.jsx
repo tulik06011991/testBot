cedc223433e628e4273fa873f571c17d411edf5b
realProject <tolqinmirsaliyev@gmail.com>
2024-05-02 12:45:02 +0500

    ghgfhg

--- frontend/src/Components/admin/GetQuestions.jsx
+++ frontend/src/Components/admin/GetQuestions.jsx
@@ -1,5 +1,7 @@
 import axios from 'axios';
 import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
+
 
 const GetQuestions = () => {
 
@@ -26,29 +28,32 @@ const GetQuestions = () => {
             <div className="container w-full ml-4">
                 <h1 className="mb-8 text-center">Savollar ro'yxati</h1>
     
-                <table className="text-left w-full">
-                    <thead className="bg-black flex text-white w-full">
-                        <tr className="flex w-full mb-4">
-                            <th className="p-4 w-2/4">Savol</th>
-                            <th className="p-4 w-1/4">Variantlar</th>
-                            <th className="p-4 w-1/4">To'g'ri javob</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {data.map((question, index) => (
-                            <tr key={index} className="flex mb-4 overflow-y-scroll w-full">
-                                <td className="p-4 w-2/4">{question.title}</td>
-                                <td className="p-4 w-1/4">
-                                    {question.options.map((variant, i) => (
-                                        <h4 key={i}>{variant}</h4>
-                                    ))}
-                                </td>
-                                <td className="p-4 w-1/4">{question.correctAnswer}</td>
+                <div className="overflow-y-scroll">
+                    <table className="text-left w-full">
+                        <thead className="bg-black flex text-white w-full">
+                            <tr className="flex w-full mb-4">
+                                <th className="p-4 w-2/4">Savol</th>
+                                <th className="p-4 w-1/4">Variantlar</th>
+                                <th className="p-4 w-1/4">To'g'ri javob</th>
                             </tr>
-                        ))}
-                    </tbody>
-                </table>
+                        </thead>
+                        <tbody>
+                            {data.map((question, index) => (
+                                <tr key={index} className="flex mb-4 w-full">
+                                    <td className="p-4 w-2/4">{question.title}</td>
+                                    <td className="p-4 w-1/4" style={{ maxHeight: '200px', overflowY: 'scroll' }}>
+                                        {question.options.map((variant, i) => (
+                                            <h4 key={i}>{variant}</h4>
+                                        ))}
+                                    </td>
+                                    <td className="p-4 w-1/4">{question.correctAnswer}</td>
+                                </tr>
+                            ))}
+                        </tbody>
+                    </table>
+                </div>
             </div>
+            <button className='d-flex  text-center items-end bg-blue-500 py-2 px-4 rounded'><Link to={'/adminpanel'}>Orqaga</Link></button>
         </div>
     );
 };
