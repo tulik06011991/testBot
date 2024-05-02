3a880de0fc77f1b6679bccada15d169bf6b58e7f
realProject <tolqinmirsaliyev@gmail.com>
2024-05-02 12:24:15 +0500

    fddf

--- frontend/src/Components/admin/GetQuestions.jsx
+++ frontend/src/Components/admin/GetQuestions.jsx
@@ -20,7 +20,37 @@ const GetQuestions = () => {
       }, []);
 
   return (
-    <div><br /><br /><br /><br /><h1>salom</h1></div>
+    <div><br /><br /><br /><br />
+    <div class="container w-full ml-4">
+        <h1 class="mb-8 text-center">
+        Savollar ro'yxati
+      </h1>
+    
+        <table class="text-left w-full">
+            <thead class="bg-black flex text-white w-full">
+                <tr class="flex w-full mb-4">
+                    <th class="p-4 w-1/4">T/r</th>
+                    <th class="p-4 w-1/4">Savol</th>
+                    <th class="p-4 w-1/4">variant</th>
+                    <th class="p-4 w-1/4">variant</th>
+                    <th class="p-4 w-1/4">variant</th>
+                    <th class="p-4 w-1/4">To'g'ri javob</th>
+                </tr>
+            </thead>
+        
+            <tbody class="bg-grey-light flex flex-col items-center justify-between overflow-y-scroll w-full" style={{"height": 50}}>
+                <tr class="flex w-full mb-4">
+                    <td class="p-4 w-1/4">Dogs</td>
+                    <td class="p-4 w-1/4">Cats</td>
+                    <td class="p-4 w-1/4">Birds</td>
+                    <td class="p-4 w-1/4">Fish</td>
+                    <td class="p-4 w-1/4">Fish</td>
+                    <td class="p-4 w-1/4">Fish</td>
+                </tr>
+                
+            </tbody>
+        </table>
+    </div></div>
   )
 }
 
