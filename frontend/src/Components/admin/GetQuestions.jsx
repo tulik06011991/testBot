caa2fa47ec0e66ad63369245532e00272fbe5450
realProject <tolqinmirsaliyev@gmail.com>
2024-05-02 12:15:18 +0500

    gfgfg

--- frontend/src/Components/admin/GetQuestions.jsx
+++ frontend/src/Components/admin/GetQuestions.jsx
@@ -1,6 +1,24 @@
-import React from 'react'
+import axios from 'axios';
+import React, { useEffect, useState } from 'react'
 
 const GetQuestions = () => {
+
+    const [data, setData] = useState([]);
+    const [message, setMessage] = useState('')
+    useEffect(() => {
+        const fetchQuestions = async () => {
+          try {
+            const response = await axios.get('http://localhost:3000/test/answer/get');
+           setData(response.data)
+           console.log(response.data)
+          } catch (error) {
+            console.error('Savollarni yuklashda xatolik:', error);
+            setMessage('Savollarni yuklashda xatolik yuz berdi');
+          }
+        };
+        fetchQuestions();
+      }, []);
+
   return (
     <div><br /><br /><br /><br /><h1>salom</h1></div>
   )
