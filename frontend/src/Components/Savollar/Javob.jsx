2423a9c5f48a738f717e46323e138efb33987ab8
tulik <tolqinmirsaliyev@gmail.com>
2024-05-03 23:50:39 +0300

    tayyor bo'ldi xudoga shukr

--- frontend/src/Components/Savollar/Javob.jsx
+++ frontend/src/Components/Savollar/Javob.jsx
@@ -40,7 +40,7 @@ const Javob = () => {
         <br /><br />
         <h2 className='text-xl font-bold'> Umumiy balingiz: {data.userScore}</h2><br /><br /><br />
         <button className='bg-blue-300  py-2 px-4 hover:bg-blue-500 text-white' onClick={handleClearLocalStorage}>
-          <Link to={'/menu'}>Orqaga</Link>
+          <Link to={'/'}>Orqaga</Link>
         </button>
       </div>
     </>
