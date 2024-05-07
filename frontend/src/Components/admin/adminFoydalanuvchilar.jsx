b8646e34945a5fb39ed0a18baf96b42d5dd7cdd7
realProject <tolqinmirsaliyev@gmail.com>
2024-05-07 10:47:43 +0500

    sss

--- frontend/src/Components/admin/adminFoydalanuvchilar.jsx
+++ frontend/src/Components/admin/adminFoydalanuvchilar.jsx
@@ -73,8 +73,8 @@ const AdminFoydalanuvchilar = () => {
                             <td className="p-2 md:border md:border-grey-500 text-left block md:table-cell">
 
                                 <span className="inline-block w-1/3 md:hidden font-bold">Actions</span>
-                                <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-1 px-2 border mr-4 border-blue-500 rounded">Edit</button>
-                                <button type='button' onClick={() => handleDelete(  localStorage.setItem('del', user.userId)   )} className="bg-red-500 hover:bg-red-700 text-white font-bold py-1 px-2 border border-red-500 rounded">Delete</button>
+                                {/* <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-1 px-2 border mr-4 border-blue-500 rounded">Edit</button>
+                                <button type='button' onClick={() => handleDelete(  localStorage.setItem('del', user.userId)   )} className="bg-red-500 hover:bg-red-700 text-white font-bold py-1 px-2 border border-red-500 rounded">Delete</button> */}
 
                                 <span className="inline-block w-1/3 md:hidden font-bold">Actions</span>                               
                                 {/* <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-1 px-2 border mr-4 border-blue-500 rounded">Edit</button> */}
